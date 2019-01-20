0aa47536f31e23bf6a9855ccd17fc2cad189d18d
wen.wu <wnnwoo@163.com>
2019-01-20 23:51:47 +0800

    add test scheme

---
+++ BlueShellTest/BlueShellTest.swift
@@ -0,0 +1,35 @@
+//
+//  BlueShellTest.swift
+//  BlueShellTest
+//
+//  Created by WnnWoo on 2019/1/20.
+//  Copyright © 2019年 WnnWoo. All rights reserved.
+//
+
+import XCTest
+
+class BlueShellTest: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssert(true)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
